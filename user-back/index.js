641fbb675dc6293b9aa0e2e037a490bf6cd2eff4
dogi26 <dagflutter26@gmail.com>
2025-07-16 23:55:48 +0300

    update backend, add config controller ...

--- user-back/index.js
+++ user-back/index.js
@@ -1,19 +1,26 @@
-// mafiaback/index.js
+// user-back/index.js
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+
+
+dotenv.config();
+connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json()); // Allows JSON body parsing
+app.use(express.json());
 
-// Sample route
-app.get('/api/hello', (req, res) => {
-    res.json({ message: 'Hello from the backend!' });
-});
+// Import routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/market', require('./routes/market'));
+app.use('/api/portfolio', require('./routes/portfolio'));
+app.use('/api/trade', require('./routes/trade'));
+app.use('/api/wallet', require('./routes/wallet'));
+app.use('/api/news', require('./routes/news'));
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
